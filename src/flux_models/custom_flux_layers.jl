3e99a033d39e2530d63ea82e1608957fee9ff4b7
Marco Molinari <marco.molinari@medisin.uio.no>
2024-02-12 10:42:31 +0100

    Tanh scale mixture extension

--- src/flux_models/custom_flux_layers.jl
+++ src/flux_models/custom_flux_layers.jl
@@ -645,4 +645,116 @@ function WeightTracking.container_dim_init(layer::TanhDense)
 end
 
 
+"""
+    TanhDenseMix(
+        (in, out)::Pair{<:Integer, <:Integer};
+        bias=true,
+        activation=identity,
+        init=Flux.glorot_normal,
+        lambda=0f0,
+        prior_scale=Distributions.TruncatedNormal(0f0, 10f0, 0f0, Inf32)
+    )
+
+    Custom Flux Scale-Mixture Dense layer
+    Works like a standard Dense layer, but creates a set of additional scale coefficients (one for each weight),
+        which are used in the weights loss function.
+"""
+struct TanhDenseMix{M <: AbstractMatrix, B, F, L<:Float32, P_W<:Distributions.Distribution}
+    weight::M
+    scale::M
+    bias::B
+    activation::F
+    lambda_weights::L
+    lambda_scales::L
+    prior_weight::P_W
+
+    function TanhDenseMix(
+        W::M,
+        S::M,
+        bias::B = true,
+        activation::F = identity,
+        lambda_weights::L=1f0,
+        lambda_scales::L=1f0,
+        prior_weight::P_W = Distributions.truncated(Distributions.Normal(0f0, 1f0), 0f0, Inf32),
+    ) where {M <: AbstractMatrix, B<:Union{Bool, AbstractArray}, F, L, P_W}
+        b = Flux.create_bias(W, bias, size(W, 1))
+        new{M, typeof(b), F, L, P_W}(W, S, b, activation, lambda_weights, lambda_scales, prior_weight)
+    end
+end
+
+TanhDenseMix(
+    (in, out)::Pair{<:Integer, <:Integer};
+    bias=true,
+    activation=identity,
+    init_weights=Utilities.init_weights,
+    init_scales=Utilities.init_scales,
+    lambda_weights=1f0,
+    lambda_scales=1f0,
+    prior_weight=Distributions.truncated(Distributions.Normal(0f0, 1f0), 0f0, Inf32),
+) = TanhDenseMix(init_weights(out, in), init_scales(out, in), bias, activation, lambda_weights, lambda_scales, prior_weight)
+
+Flux.@functor TanhDenseMix
+
+function (l::TanhDenseMix)(x::AbstractVecOrMat)
+    Flux._size_check(l, x, 1 => size(l.weight, 2))
+    xT = Flux._match_eltype(l, x)
+
+    weight_pos = Flux.softplus.(l.weight)
+    scale_tanh = Flux.tanh_fast.(l.scale)
+
+    return l.activation(scale_tanh .* weight_pos * xT .+ l.bias)
+end
+
+function Base.show(io::IO, l::TanhDenseMix)
+    println(io, "TanhDenseMix(")
+    println(io, "\t", size(l.weight, 2), " => ", size(l.weight, 1), ";")
+    println(io, "\t", "activation = ", l.activation, ",")
+    println(io, "\t", "bias = ", l.bias, ",")
+    println(io, "\t", "lambda_weights = ", l.lambda_weights, ",")
+    println(io, "\t", "lambda_scales = ", l.lambda_scales, ",")
+    println(io, "\t", "prior_weight = ", l.prior_weight, ",")
+    print(io, ")")
+end
+
+# Add penalty for ScaleMixtureDense layer - Horseshoe hierachical structure here
+function Penalties.penalty(l::TanhDenseMix)
+    # transform the scale parameters first
+    weight_pos = Flux.softplus.(l.weight)
+    scale_tanh = Flux.tanh_fast.(l.scale)
+    prior_scale = Distributions.truncated.(Distributions.Normal.(0f0, weight_pos), -1f0, 1f0)
+
+    -(
+        l.lambda_scales * sum(Distributions.logpdf.(prior_scale, scale_tanh)) +
+        l.lambda_weights * sum(Distributions.logpdf.(l.prior_weight, weight_pos))
+    )
+end
+
+# Extend weights tracking function
+function WeightTracking.weight_container_init(layer::TanhDenseMix; n_iter::Int64)
+    w_dict = Dict()
+    for (pos, param) in enumerate(Flux.params(layer))
+        param_size = size(param)
+        w_dict[string(pos)] = zeros32(param_size..., n_iter)
+    end
+
+    return w_dict
+end
+
+function WeightTracking.weight_tracking_push!(epoch::Int64, layer::TanhDenseMix, dict_weights_layer::Dict, dict_dims_layer::Dict)
+    for (pos, param) in enumerate(Flux.params(layer))
+        dict_weights_layer[string(pos)][dict_dims_layer[string(pos)]..., epoch] = param
+    end
+end
+
+function WeightTracking.container_dim_init(layer::TanhDenseMix)
+    dim_dict = Dict()
+    for (pos, param) in enumerate(Flux.params(layer))
+        param_size = size(param)
+        dim_dict[string(pos)] = ntuple(_ -> (:), length(param_size))
+    end
+
+    return dim_dict
+end
+
+
 end
